d2ed9e5e15a06754660b40712958432005cd15e4
조성국 <ksturtle2@naver.com>
2025-05-17 16:35:46 +0900

    feat: JSX 및 JSX내 JS 사용, 중괄호 사용 방법 예제 refactor: 중요 요약 내용 noetpad.txt로 이동 및 추가

--- learn-react/src/components/Counter.jsx
+++ learn-react/src/components/Counter.jsx
@@ -1,28 +1,5 @@
-/*
-
-// 함수 선언식 : 컴포넌트 상태, 로직인 경우 권유
-
-export default function Counter() {  // ! 반드시 Pascal 함수명은 Case로 작성
-    return (  // 컴포넌트 호출 시 Return 할 HTML 작성
-        <button>Counter</button>
-    )
-}
-
-*/
-
-
-
-/*
-* 함수에 export default를 통해
-*
-* */
-
-// 함수 표현식 : UI만 담당하는 경우
-// 상태를 사용하지 않는 컴포넌트에 일반적으로 사용
 export const Counter = () => {
     // return ( // => return 문 생략 가능
       <button>Counter</button>
     // )
 }
-
-// export default Counter;
