4a69616ed263abd6ab488ea1bb8561c0efa2fb11
조성국 <ksturtle2@naver.com>
2025-05-25 00:36:11 +0900

    feat: 조건부 랜더링

--- learn-react/src/components/course/CourseItem.jsx
+++ learn-react/src/components/course/CourseItem.jsx
@@ -1,4 +1,34 @@
-export default function CourseItem({title, description, thumbnail}) {
+function HeartIconBtn({ isFavorite = false}) {
+
+  return (
+    <button className="btn">
+      {/*
+      {
+        isFavorite ? (
+          <img className="icon-heart" src="./img/heart-fill-icon.png" alt="하트 이미지"/>
+        ) : (
+          <img className="icon-heart" src="./img/heart-icon.png" alt="하트 이미지"/>
+        )
+      }
+      */}
+      <img className="btn__img" src={isFavorite ? "./img/heart-fill-icon.png" : "./img/heart-icon.png"} alt="하트 이미지"/>
+    </button>
+  )
+
+
+}
+
+function LinkIconBtn({ link }) {
+
+  return (
+    <a className="btn" href={link} target="_blank" rel="noopener noreferrer">
+      <img className="btn__img" src="./img/link-icon.png" alt="링크 이미지"/>
+    </a>
+  )
+
+}
+
+export default function CourseItem({title, description, thumbnail, isFavorite, link}) {
 
   const isEmpty = false;
 
@@ -14,6 +44,13 @@ export default function CourseItem({title, description, thumbnail}) {
       <div className="course__body">
         <div className="course__title">{title}</div>
         <div className="course__description">{description}</div>
+      </div>
+
+      <div className="course__icons">
+          <HeartIconBtn isFavorite={isFavorite} />
+          {
+            link && <LinkIconBtn link={link} />   /* 논리 연산자에 의해 link가 존재해야 a 태그(LinkIconBtn 컴포넌트) 랜더링 */
+          }
       </div>
     </article>
   );
