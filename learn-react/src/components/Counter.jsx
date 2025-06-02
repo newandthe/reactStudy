271344795c5b693c09959513c95418a24a2a01ca
조성국 <ksturtle2@naver.com>
2025-06-02 14:31:32 +0900

    feat: 스냅샷처럼 동작하는 State

--- learn-react/src/components/Counter.jsx
+++ learn-react/src/components/Counter.jsx
@@ -3,10 +3,16 @@ import {useState} from "react";
 
 export default function Counter({ onTotal }) {
 
-  const [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(0);
+
+  console.log('[랜더링] Counter : ', counter);
 
   const handleCounter = () => {
     setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    console.log('[함수호출] Counter : ', counter);
+
     if (onTotal) {
       onTotal();
     }
--- learn-react/src/components/Main.jsx
+++ learn-react/src/components/Main.jsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 export default function Main() {
 
   const [total, setTotal] = useState(0);
+  const [flag, setFlag] = useState(false);
 
   const handleTotal = () => {
     setTotal(total + 1);
@@ -12,6 +13,9 @@ export default function Main() {
   return (
     <main>
       <h2>totalCounter: {total} </h2>
+      <h2>flag: {flag.toString()}</h2>
+      <button onClick={() => setFlag(!flag)}>toggle flag</button>
+      <hr />
       <Counter onTotal={handleTotal}/>
       <hr/>
       <Counter onTotal={handleTotal}/>
