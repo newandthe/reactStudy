98274bcf41aa76d41a778de90527ac558efe6933
조성국 <ksturtle2@naver.com>
2025-06-05 19:16:49 +0900

    feat: 배열 State 업데이트 (1)

--- learn-react/src/AppTodo.jsx
+++ learn-react/src/AppTodo.jsx
@@ -2,15 +2,71 @@ import { useState } from 'react';
 import './App.css'
 import TodoList from "./todo/TodoList.jsx";
 function AppTodo() {
-  const [todos] = useState([
-    {id: 0, label: "HTML&CSS 공부하기" },
-    {id: 1, label: "자바스크립트 공부하기" },
+  const [todoText, setTodoText] = useState('');
+  const [todos, setTodos] = useState([
+    {id: 0, text: "HTML&CSS 공부하기", done: false },
+    {id: 1, text: "자바스크립트 공부하기", done: false },
   ]);
+
+  const handleTodoTextChange = (e) => {
+    setTodoText(e.target.value);
+  }
+
+  const handleApppTodo = () => {
+
+    const nextId = todos.length;
+
+    setTodos([
+      ...todos,
+      { id: nextId, text: todoText }
+    ])
+
+    setTodoText('');  /* 기존 input 초기화 // null 및 uundefined는 동작 X */
+
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleApppTodo();
+    }
+  }
+
+  const handleDeleteTodo = (deleteId) => {
+    /* 중간에 있는 배열을 빼기 위해서는 filter, slice 함수가 유용하다. */
+    const newTodos = todos.filter(item => item.id !== deleteId);
+    setTodos(newTodos);
+  }
+
+  const handleToggleTodo = (id, done) => {
+    /* 기존 배열 안의 객체 속성을 변경 */
+    const nextTodos = todos.map(item => {
+      if (item.id === id) {
+        return { ...item, done };
+      }
+
+      return item;
+    })
+
+    setTodos(nextTodos);
+  }
+
+
   return (
     <div>
       <h2>할일목록</h2>
-      <TodoList todos={todos} />
-      <TodoList todos={todos} />
+      <input id={"todoTextInput"}
+             type="text"
+             value={todoText}
+             onChange={handleTodoTextChange}
+             onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleApppTodo}>추가</button>
+      <div>Preview: {todoText}</div>
+      <TodoList
+        todos={todos}
+        onDeleteTodo={handleDeleteTodo}
+        onToggleTodo={handleToggleTodo}
+      />
     </div>
   );
 }
--- learn-react/src/todo/TodoList.jsx
+++ learn-react/src/todo/TodoList.jsx
@@ -1,10 +1,20 @@
-function TodoList({ todos = []}) {
+function TodoList({ todos = [], onDeleteTodo, onToggleTodo}) {
   const items = [...todos];
-  items.push({ id: 2, label: '포트폴리오 사이트 만들기'});
+  // items.push({ id: 2, label: '포트폴리오 사이트 만들기'});
 
   return (
     <ul>
-      {items.map(item =>  <li key={item.id}>{item.label}</li>)}
+      {items.map(item => (
+        <li key={item.id}>
+          <input
+            type={"checkbox"}
+            checked={item.done}
+            onChange={(e) => onToggleTodo(item.id, e.target.checked)}
+          />
+          <span>{item.done ? (<del>{item.text}</del>) : item.text}</span>
+          <button onClick={() => onDeleteTodo(item.id)}>X</button>  {/* 항목 제거 버튼 */}
+        </li>
+      ))}
     </ul>
   )
 }
