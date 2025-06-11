d560d5473857f371be53b95930042fc55f28b5d4
조성국 <ksturtle2@naver.com>
2025-06-11 20:28:29 +0900

    refactor: Context Hook 을 통한 상태 공유 (2) - Provider 및 State 를 Context에 작성

--- learn-react/src/components/theme/Main.jsx
+++ learn-react/src/components/theme/Main.jsx
@@ -1,11 +1,14 @@
 import Card from '../Card';
+import {DarkModeProvider} from "../../context/DarkModeContext.jsx";
 
 export default function Main() {
   return (
     <main>
-      <Card title="제목">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus iste laborum iusto ipsa cum est dolorum pariatur eaque ipsam at, sunt exercitationem dolor magnam, et esse. Porro laborum eligendi nemo.
-      </Card>
+      <DarkModeProvider>
+        <Card title="제목">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus iste laborum iusto ipsa cum est dolorum pariatur eaque ipsam at, sunt exercitationem dolor magnam, et esse. Porro laborum eligendi nemo.
+        </Card>
+      </DarkModeProvider>
     </main>
   );
 }
--- learn-react/src/context/DarkModeContext.jsx
+++ learn-react/src/context/DarkModeContext.jsx
@@ -1,3 +1,18 @@
-import { createContext } from "react";
+import {createContext, useState} from "react";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const DarkModeContext = createContext({});
+
+export const DarkModeProvider = ({ children, initDarkMode = true }) => {
+
+  const [darkMode, setDarkMode] = useState(initDarkMode);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  )
+}
