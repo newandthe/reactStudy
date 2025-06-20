736e79d95b34119dc53e06d13bd183988e04564c
조성국 <ksturtle2@naver.com>
2025-06-20 18:35:28 +0900

    feat: 메모이제이션 - memo (function)

---
+++ learn-react/src/AppMemo.jsx
@@ -0,0 +1,80 @@
+import {useState, memo, useMemo} from "react";
+// 메모이제이션이 적용되지 않은 컴포넌트
+const RegularComponent = ({ count, items = [] }) => {
+  console.log('RegularComponent 렌더링');
+
+  return (
+    <fieldset>
+      <legend>일반 컴포넌트</legend>
+      <div>{count}</div>
+      <ul>
+        {items.map(item => (<li key={item.id}>{item.text}</li>))}
+      </ul>
+    </fieldset>
+  );
+};
+
+// 메모이제이션이 적용된 컴포넌트
+const MemoizedComponent = memo(({ count, items = [] }) => {
+  console.log('MemoizedComponent 렌더링');
+  return (
+    <fieldset>
+      <legend>메모이제이션 컴포넌트</legend>
+      <div>{count}</div>
+      <ul>
+        {items.map(item => (<li key={item.id}>{item.text}</li>))}
+      </ul>
+    </fieldset>
+  );
+});
+
+export default function AppMemo() {
+  const [count, setCount] = useState(0);
+  const [otherState, setOtherState] = useState(0);
+
+  const courses = useMemo(() => {
+    return [
+      {id: 0, text: "HTML&CSS", level: 0 },
+      {id: 1, text: "자바스크립트", level: 0 },
+      {id: 2, text: "리액트", level: 0 },
+      {id: 3, text: "리액트 중급", level: 1 },
+    ]
+  }, []);
+
+
+
+  /* 주의 obj는 primitive 타입이 아니라 같은 내용을 가진 각각의 obj1 !== obj2 (바라보는 주소 값이 달라서..) 새로운 객체를 생성해서 사용하는 경우 useMemo를 사용해야함 */
+  /*
+  const courses = [
+    {id: 0, text: "HTML&CSS", level: 0 },
+    {id: 1, text: "자바스크립트", level: 0 },
+    {id: 2, text: "리액트", level: 0 },
+    {id: 3, text: "리액트 중급", level: 1 },
+  ];
+  */
+
+  /*
+  // 메모이제이션 useState Obj 예제
+  const [courses, setCourses] = useState([
+    {id: 0, text: "HTML&CSS", level: 0 },
+    {id: 1, text: "자바스크립트", level: 0 },
+    {id: 2, text: "리액트", level: 0 },
+    {id: 3, text: "리액트 중급", level: 1 },
+  ]);
+
+  const beginnerCourses = useMemo(() => {
+    return courses.filter((course) => course.level === 0);
+  }, [courses])
+  */
+
+  return (
+    <div>
+      <h2>컴포넌트 메모이제이션</h2>
+      <button onClick={() => setCount(count + 1)}>카운트 증가</button>
+      <button onClick={() => setOtherState(otherState + 1)}>기타 상태 변경</button>
+      <hr />
+      <RegularComponent count={count} items={courses} />
+      <MemoizedComponent count={count} items={courses} />
+    </div>
+  )
+}
