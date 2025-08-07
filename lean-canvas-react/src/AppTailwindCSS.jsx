08bb60b906dc1cad7f78cd6e51dc4101054dfc33
조성국 <ksturtle2@naver.com>
2025-08-07 21:52:00 +0900

    Setting: TailWindCSS 설치 및 테스트

---
+++ lean-canvas-react/src/AppTailwindCSS.jsx
@@ -0,0 +1,14 @@
+import TailwindButton from './components/TailwindCSS/TailwindButton.jsx';
+
+function AppTailwindCss() {
+  return (
+    <>
+      <h1 className="text-sky-300 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <TailwindButton>TailwindButton</TailwindButton>
+    </>
+  );
+}
+
+export default AppTailwindCss;
